c9a46cb8ca844fef5923184d6631c056d09d7664
samirvithlani <samir.vithlani83955@gmail.com>
2023-07-22 14:30:14 +0530

    add

--- app.js
+++ app.js
@@ -6,6 +6,37 @@ const PORT = 3000;
 
 //server start....
 
+//localhostL:3000/test
+var users = [
+    {
+        name:"abc",
+        age:20
+    },{
+        name:"xyz",
+        age:30
+    }
+]
+app.get("/test",(req,res)=>{
+    console.log("test api called");
+    res.send("test api called");
+})
+
+app.get("/test1",(req,res)=>{
+    res.json({
+        message:"ok",
+        
+    })
+})
+
+app.get("/test2",(req,res)=>{
+    res.status(201).json({
+        message:"data fetched successfully",
+        data:users
+    })
+})
+
+
+
 mongoose.connect("mongodb://127.0.0.1:27017/cw1", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
