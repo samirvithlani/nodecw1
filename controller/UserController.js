d823ea167029d1e472ea88129adabe8e35b86d4d
samirvithlani <samir.vithlani83955@gmail.com>
2023-07-30 13:02:38 +0530

    delete update

--- controller/UserController.js
+++ controller/UserController.js
@@ -36,7 +36,74 @@ const addUser = (req,res)=>{
 
 }
 
+// const deleteUser = (req,res)=>{
+
+//     var id = req.params.id;
+//     userSchema.findByIdAndDelete(id).then((data)=>{
+//         res.status(200).json({
+//             message:"user deleted",
+//             data:data
+//         })
+//     }).catch((err)=>{
+//         res.status(500).json({
+//             message:"error",
+//             error:err
+//         })
+//     })
+
+// }
+
+const deleteUser = (req,res)=>{
+
+    var id = req.params.id;
+    userSchema.findById(id).then((data)=>{
+        if(data){
+            userSchema.findByIdAndDelete(id).then((data)=>{
+                res.status(200).json({
+                    message:"user deleted",
+                    data:data
+                })
+            }).catch((err)=>{
+                res.status(500).json({
+                    message:"error",
+                    error:err
+                })
+            })
+        }
+        else{
+            res.status(404).json({
+                message:"user not found"
+            })
+        }
+    }).catch((err)=>{
+        res.status(500).json({
+            message:"error",
+            error:err
+        })
+    })
+}
+
+const updateUser = (req,res)=>{
+
+
+    var id = req.params.id; //id
+    var data = req.body; ///update data
+    userSchema.findByIdAndUpdate(id,req.body).then((data)=>{
+        res.status(200).json({
+            message:"user updated",
+        })
+    }).catch((err)=>{
+        res.status(500).json({
+            message:"error",
+            error:err
+        })
+    })
+
+}
+
 module.exports = {
     getUsers,
-    addUser
+    addUser,
+    deleteUser,
+    updateUser
 }
--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const userController = require("../controller/UserController");
 router.get('/user',userController.getUsers);
 router.post('/user',userController.addUser)
+router.delete('/user/:id',userController.deleteUser)
+router.put('/user/:id',userController.updateUser)
 
 module.exports = router;
