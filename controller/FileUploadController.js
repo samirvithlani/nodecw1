0179495e8303b62c99a506ac394bdefee3c4efeb
samirvithlani <samir.vithlani83955@gmail.com>
2023-09-16 10:59:36 +0530

    filter multer

--- controller/FileUploadController.js
+++ controller/FileUploadController.js
@@ -2,7 +2,7 @@ const multer = require("multer");
 const path = require("path");
 
 const storage = multer.diskStorage({
-  destination: './uploads/',
+  destination: "./uploads/",
 
   filename: function (req, file, cb) {
     cb(null, file.originalname);
@@ -14,16 +14,28 @@ const upload = multer({
   limits: {
     fileSize: 1024 * 1024 * 5,
   },
-  // fileFilter: function(req, file, cb){
-  //     checkFileType(file, cb);
-  // }
+  fileFilter: function (req, file, cb) {
+    console.log("file", cb);
+    checkFileType(file, cb);
+  },
 }).single("file");
 
+const checkFileType = (file, cb) => {
+  const fileTypes = /jpeg|jpg|png|gif/;
+  const extName = fileTypes.test(path.extname(file.originalname).toLowerCase());
+  const mimeType = fileTypes.test(file.mimetype);
+  if (extName && mimeType) {
+    return cb(null, true);
+  } else {
+    cb("Error: Images Only!");
+  }
+};
+
 const uploadFile = (req, res) => {
-    
   upload(req, res, (err) => {
+    console.log("req.file", req.file);
     if (err) {
-      res.status(400).json({ message: err.message });
+      res.status(400).json({ message: err });
     } else {
       res.status(200).json({
         message: "File uploaded successfully",
